7b5df541a83ea2d718840b04f0ef1e090251fdc6
ZJUZSY <zjuzsy2015@zju.edu.cn>
2017-10-16 13:49:40 +0800

    全部提交，现在学习撤销操作

--- hello1.js
+++ hello1.js
@@ -1,11 +1,10 @@
 ﻿console.log('Hello NodeJS');
 console.log('Hello NodeJS');
 console.log('Hello NodeJS');
-console.log('Hello NodeJS');
-console.log('Hello NodeJS');
+
 function(){
 	var a = 1;
 	var b = 2;
-	var c = 3;
+	var c = 1;
 }
 
