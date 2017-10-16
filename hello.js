c6a21011ace52765f7f5e7ef711f8f04b34a5a88
ZJUZSY <zjuzsy2015@zju.edu.cn>
2017-10-16 17:27:30 +0800

    new1

---
+++ hello.js
@@ -0,0 +1,12 @@
+﻿console.log('Hello NodeJS');
+console.log('Hello NodeJS');
+console.log('Hello NodeJS');
+console.log('Hello NodeJS');
+console.log('Hello NodeJS');
+function(){
+	var a = 1;
+	var b = 2;
+	var c = 3;
+}
+var new = 1;
+
